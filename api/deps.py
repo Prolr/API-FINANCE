d4fce397d0e11d75d53a6de662d190a543e374b2
unknown <Rafael Silva>
2025-09-26 12:08:50 -0300

    refactor: improve auth

--- api/deps.py
+++ api/deps.py
@@ -1,9 +1,19 @@
 import logging
 from typing import AsyncGenerator, Generator
 
+from fastapi import HTTPException
+from grpc import Status
+from requests import Session
+from core. import oauth2_schema
 from db.session import SessionLocal_212
 from db.session import SessionLocal_211
 from db.session import SessionLocal_psql
+from pydantic import BaseModel
+from typing import Optional
+from sqlalchemy import Asyn
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import HTTPException, status
+from models.model_user import User
 
 
 async def get_db_psql() -> AsyncGenerator:
@@ -29,3 +39,42 @@ def get_db_212() -> Generator:
 
 # Alias para não quebrar os endpoints que usam `Depends(get_db)`
 get_db = get_db_psql
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+
+async def get_session() -> AsyncGenerator:
+    session: AsyncSession = session
+
+    try:
+        yield session
+    finally:
+        await session.close()
+
+
+async def get_current_user(
+    db: Session = Depends(get_session),
+    token: str = Depends(oauth2_schema)
+
+) -> UsuarioModel:
+    credential_exception: HTTPException = HTTPException
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = ('não foi possivel autenticar sua  credencial'
+              headers={"WWWE-Autenticar": "Bearer"},
+
+              )
+
+try:
+    payload = jwt.decode(
+        token,
+        settings.JWT_SECRETS,
+        options={"verify_aud ": False}
+    )
+username: str = payload.get(" sub")
+if username is None:
+    raise credential_exeption
+
+async with db as session:
+    query = select(User).filter(username.id == int(token_data_Username))
--- core/security.py
+++ core/security.py
@@ -1,11 +1,23 @@
 
 
 # security.py → Funções de autenticação e geração de tokens(JWT).
-
 import secrets
 from core.config import Settings
 from pydantic_settings import BaseSettings
 from passlib.context import CryptContext
+from pytz import timezone
+from typing import Optional, List
+from datetime import datetime, timedelta
+from fastapi.security import OAuth2AuthorizationCodeBearer
+from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from jose import jwt
+import models.model_user import Base
+from core.config import settings
+from core.config import verificar_senha
+from pydantic import EmailStr
+from models.model_user import User
+
 
 CRIPTO = CryptContext(schemas=['bcrypt'], deprecated='auto')
 
@@ -19,3 +31,48 @@ def verificar_senha(senha: str, has_senha: str) -> bool:
 def gerar_hash_senha(senha: str) -> str:
 
     return CRIPTO.hash(senha)
+
+
+# Verificação
+
+oauth2_schemas = OAuth2AuthorizationCodeBearer(
+    tokenUrl="{setting.API_V1_STR}/usuarios/login"
+)
+
+
+async def autenticar(email: EmailStr, senha: str, db: AsyncSession) -> Optional[User]:
+    async with db as session:
+        query = select(User).filter(User.email == email)
+        result = await session.execute(query)
+        usuario = User = result.scalar().unique().one_or_none()
+
+    if not usuario:
+        return None
+
+    if not verificar_senha(senha, usuario.senha):
+        return None
+    return usuario
+
+
+def _criar_token(tipo_token: str, tempo_vida: timedelta, sub: str) -> str:
+    payload = {}
+    sp = timezone('America/São Paulo')
+    expira = datetime.now(tz=sp) + tempo_vida
+
+    payload['type'] = tipo_token
+    payload['exp'] = expira
+    payload['iat'] = datetime.now(tz=sp)
+    payload['sub'] = str(sub)
+    return jwt.encode(payload, settings.JWT.SECRETS, algorithm=settings.ALGORITHM)
+
+
+def criar_token_acesso(subs: str) -> str:
+    '''
+    https://jwt.io
+    '''
+    return _criar_token(
+        tipo_token='access_token',
+        tempo_vida=timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES),
+        subs=subs
+
+    )
